6d1a7f6ef675cc1db18076caf6d8360db8f1ebb9
jinno Yun <jinno321@naver.com>
2024-01-12 17:15:08 +0900

    feat: roomSchema

---
+++ database/models/room.js
@@ -0,0 +1,19 @@
+const mongoose = require('connect')
+
+const roomSchema = new mongoose.Schema({
+    roomId: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    userId: {
+        type: String,
+        required: true,
+        ref: 'User', //FK
+    },
+    roomName: {
+      type: String,
+      required: true,
+    },
+  });
+  
