c2a467fa8b056d0358d214f17e0ceb961f0e4347
jinno Yun <jinno321@naver.com>
2024-01-16 20:10:32 +0900

    fix: file naming

--- src/api/home/homeRouter.js
+++ src/api/home/homeRouter.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const roomController = require('./homeController');
 
-router.post('/addRoom', roomController.createRoom);
+router.post('/addRoom', roomController.addRoom);
 router.get('/',roomController.getRoomList);
 router.get('/getSunriseTime',roomController.getSunriseTime);
 router.get('/getSunsetTime',roomController.getSunsetTime);
