66bacfbc883334ff2c204eb45516fb6d6cfb45ca
jinno Yun <jinno321@naver.com>
2024-01-16 19:38:56 +0900

    feat: addRoomApi

--- src/api/home/homeController.js
+++ src/api/home/homeController.js
@@ -1,1 +1,39 @@
-//still
+const roomService = require('../services/homeService');
+
+const addRoom = async (req, res, next) => {
+  try {
+    const { userId, roomName } = req.body;
+    const result = await roomService.createRoom(userId, roomName);
+    res.status(201).json(result);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const getRoomList = async (req, res, next) => {
+  try {
+    const rooms = await roomService.getRoomList();
+    res.status(200).json(rooms);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const getSunriseTime = (req, res) => {
+  // 여기에 일출 시간을 얻는 기능 추가
+  // 어떻게 얻을지에 따라 구현이 달라질 수 있습니다.
+  res.status(200).json({ sunriseTime: '07:00 AM' });
+};
+
+const getSunsetTime = (req, res) => {
+  // 여기에 일몰 시간을 얻는 기능 추가
+  // 어떻게 얻을지에 따라 구현이 달라질 수 있습니다.
+  res.status(200).json({ sunsetTime: '06:00 PM' });
+};
+
+module.exports = {
+  addRoom,
+  getRoomList,
+  getSunriseTime,
+  getSunsetTime,
+};
--- src/api/home/homeService.js
+++ src/api/home/homeService.js
@@ -1,1 +1,36 @@
-//still
+const Room = require('../models/roomModel');
+const User = require('../models/userModel');
+
+const addRoom = async (userId, roomName) => {
+  const user = await User.findById(userId);
+  if (!user) {
+    const error = new Error('User not found');
+    error.status = 404;
+    throw error;
+  }
+
+  const newRoom = new Room({
+    userId,
+    roomName,
+    isRoomLightOn: false,
+    isAutoMode: false,
+  });
+
+  const savedRoom = await newRoom.save();
+  user.rooms.push(savedRoom._id);
+  await user.save();
+
+  return {
+    roomId: savedRoom._id,
+    roomName: savedRoom.roomName,
+  };
+};
+
+const getRoomList = async () => {
+  return await Room.find();
+};
+
+module.exports = {
+  addRoom,
+  getRoomList,
+};
