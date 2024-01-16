b8ddf6737bbc16d0b387b41cfec27e32b646265f
jinno Yun <jinno321@naver.com>
2024-01-16 14:40:01 +0900

    feat: deviceValueSchema 생성

---
+++ database/schemas/deviceValueSchema.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const deviceValueSchema = new mongoose.Schema({
+    deviceValueId: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    deviceId: { 
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Device',
+    },
+    value: {
+      type: Number,
+      required: true,
+      min: 0,
+      max: 100
+    },
+    updateDate: {
+         type: Date, default: Date.now 
+    },
+  });
+
+module.exports = deviceSchema;
+  
