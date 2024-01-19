eef54dfaa4460177e979ae1a6271858b8ca05596
jinno Yun <jinno321@naver.com>
2024-01-19 18:24:32 +0900

    add: deviceCategory-Sensor

--- src/api/device/deviceService.js
+++ src/api/device/deviceService.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const Device = require('../../database/models/deviceModel');
-const DeviceValue = require('../../database/models/deviceValueModel');
 
 /**
  * 
