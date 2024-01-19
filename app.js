8a9c973a251ff69fe3d696d6c39cd456ea22127f
jinno Yun <jinno321@naver.com>
2024-01-19 19:14:54 +0900

    feat: error confix

--- app.js
+++ app.js
@@ -3,12 +3,11 @@ const dotenv = require('dotenv');
 dotenv.config();
 const MQTT_PORT = process.env.MQTT_PORT;
 const HTTP_PORT = process.env.HTTP_PORT;
-const HOST = process.env.HOST;
+const HOST =  process.env.HOST;
 
 //mqtt connection
 const mqtt = require('mqtt'); 
 const client = mqtt.connect(`mqtt://${HOST}:${MQTT_PORT}`) 
-
 //make express server 
 const express = require('express');
 const app = express();
@@ -20,7 +19,7 @@ const roomController = require('./src/api/room/roomController');
 const deviceController = require('./src/api/device/deviceController');
 module.exports = { app,
     client
-};
+};2
 
 //Controller Mapping
 app.use('/', homeController);
